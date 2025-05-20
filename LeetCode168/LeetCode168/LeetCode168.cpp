866d63fb0c7e3baf82a46d1fdd9e4bdc0d00af21
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-05-21 00:52:38 +0800

    Add files via upload

---
+++ LeetCode168/LeetCode168/LeetCode168.cpp
@@ -0,0 +1,16 @@
+﻿#include <unordered_map>
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size() / 2;
+        unordered_map<int, int> freq;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            freq[num]++;
+            if (freq[num] > n) {
+                return num;
+            }
+        }
+        return -1;
+    }
+};
