49b12047d7055d5963c9875bb37e380100333d0d
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-05-22 02:16:56 +0800

    Add files via upload

---
+++ 121/121/121/121.cpp
@@ -0,0 +1,12 @@
+﻿class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            minPrice = min(price, minPrice);
+            profit = max(profit, price - minPrice);
+        }
+        return profit;
+    }
+};
