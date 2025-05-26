4a379602ae1b8f2dc1afff9334107a034c33e830
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-05-27 00:51:39 +0800

    Add files via upload

---
+++ lengthOfLastWord58/lengthOfLastWord58/lengthOfLastWord58.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ' ') {
+                count++;
+            }
+            else if (s[i] == ' ' && s[i + 1] != ' ') {
+                if (i + 1 >= s.size()) {
+                    break;
+                }
+                count = 0;
+
+            }
+        }
+        return count;
+    }
+};
