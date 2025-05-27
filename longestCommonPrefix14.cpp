81990f0d055efb2bdab5801695a239508dae50c6
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-05-27 23:57:52 +0800

    Add files via upload

---
+++ longestCommonPrefix14.cpp
@@ -0,0 +1,41 @@
+﻿class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string finalresult = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            string st1 = strs[0];
+            string st2 = strs[i];
+            string result;
+
+            int minlen = min(strs[0].size(), strs[i].size());
+            for (int j = 0; j < minlen; j++) {
+                if (st1[j] == st2[j]) {
+                    result = result + st1[j];
+                }
+                else break;
+            }
+            if (finalresult.size() > result.size()) {
+                finalresult = result;
+            }
+            result = "";
+        }
+        return finalresult;
+    }
+};//sol 1
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+        string a = strs.front();
+        string b = strs.back();
+        string result;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] == b[i]) {
+                result += a[i];
+            }
+            else break;
+        }
+        return result;
+    }
+};//sol 2
