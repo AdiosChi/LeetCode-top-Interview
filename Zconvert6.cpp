f1b9f17a343ddf3f4391eb00b95b451d75f2822c
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-07-03 19:27:31 +0800

    Add files via upload
    網上的flag作法

---
+++ Zconvert6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows < 2) {
+            return s;
+        }
+        vector<string>rows(numRows);
+        int i = 0, flag = -1;
+        for (char c : s) {
+            rows[i].push_back(c);
+            if (i == 0 || i == numRows - 1) {
+                flag = -flag;
+            }
+            i += flag;
+        }
+        string ans;
+        for (auto row : rows) {
+            ans += row;
+        }
+        return ans;
+    }
+};
