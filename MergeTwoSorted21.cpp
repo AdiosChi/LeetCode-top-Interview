c2f7f0b8af560cb2b890a951d045f18e2a99eae8
AdiosChi <147239711+AdiosChi@users.noreply.github.com>
2025-08-22 00:20:54 +0800

    Add files via upload
    大概了解該怎麼寫list了

---
+++ MergeTwoSorted21.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode t(0);
+        ListNode* a = &t;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                a->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                a->next = list2;
+                list2 = list2->next;
+            }
+            a = a->next;
+        }
+        a->next = (list1 != nullptr) ? list1 : list2;
+        return t.next;
+
+    }
+};
